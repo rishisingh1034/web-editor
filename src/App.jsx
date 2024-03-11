880bdaaf88f45656e8707bfa6c26eac1b8e99442
Rishi Kumar Singh <rishisingh43210@gmail.com>
2024-03-11 23:49:23 +0530

    added context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Home from './components/Home'
+import DataProvider from './context/DataProvider'
 
 function App() {
 
   return (
     <>
+    <DataProvider>
     <Home/>
+    </DataProvider>
     </>
   )
 }
